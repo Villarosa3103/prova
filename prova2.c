e6e51ec8ccdd64e2006c31805cc8167ecfb68696
Villarosa3103 <125254691+Villarosa3103@users.noreply.github.com>
2023-03-06 07:32:24 +0100

    Update prova2.c

--- prova2.c
+++ prova2.c
@@ -1,15 +1,6 @@
-main()
-{
-    int I, J;
-    
-    /* per I che va da 1 a 10*/
-    for( I = 1 ; I <= 10 ; I = J + 1 ){
-        /* per J che va da 1 a 10*/
-        for( J = 1 ; J <= 10 ; J = J + 1 )
-            /* stampare I * J
-               stampare uno spazio*/
-            printf("%3d ", I*J);
-        /* andare a capo*/
-        printf("\n");
-    }
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
 }
